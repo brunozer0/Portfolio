c8ecf12fd8122b24c78cb0fe3bf2ee6693c20e70
Bruno Magalhães <106349470+brunozer0@users.noreply.github.com>
2022-08-04 17:48:27 -0300

    Atualização
    Atualização portfólio, quase lá

--- main.js
+++ main.js
@@ -1,4 +1,78 @@
+window.addEventListener ('scroll', onScroll)
 
+onScroll() 
+
+function onScroll() {
+    showNavOnScroll()
+    showBackToTopButtonOnScroll()
+
+    activateMenuAtCurrentSection(inicio)
+    activateMenuAtCurrentSection(sobremim)
+    activateMenuAtCurrentSection(habilidades)
+    activateMenuAtCurrentSection(projetos)
+    activateMenuAtCurrentSection(contato)
+    
+  }
+
+function activateMenuAtCurrentSection(section) {
+
+  //linha alvo
+  const targetLine = scrollY + innerHeight / 2
+  
+   // verificar se a seção passou da linha // quais dados vou precisar para isso? //
+
+   //O topo da seção
+   const sectionTop = section.offsetTop
+
+       // a altura da seção
+   const sectionHeight = section.offsetHeight
+   
+   //O topo da seção chegou ou ultrapassou a linha alvo//
+   const SectionTopReachOrPassedTargetLine = targetLine >=sectionTop
+
+   //informações dos dados lógicos //
+   console.log('O topo da seção chegou ou passou da linha ?', SectionTopReachOrPassedTargetLine)
+   
+   //verificar se a base está abaixo da linha alvo// quais dados irei precisar para isso?
+
+   //a seção termina onde ?//
+   const sectionEndsAt = sectionTop + sectionHeight
+
+   /// O final da seção passou da linha alvo///
+   const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+    console.log('O fundo da seção passou da linha?', sectionEndPassedTargetline)
+
+    //limites da seção//
+    const sectionBoundaries = SectionTopReachOrPassedTargetLine &&
+    !sectionEndPassedTargetline
+
+      const sectionId =  section.getAttribute('id')
+
+   const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+    menuElement.classList.remove('active')
+    if (sectionBoundaries) {
+      menuElement.classList.add('active')
+    }
+   
+}
+
+
+  function showNavOnScroll() {
+    if (scrollY > 0) {
+        navegacao.classList.add('scroll')
+      } else {
+        navegacao.classList.remove('scroll')
+      }
+  }
+  function showBackToTopButtonOnScroll() {
+    if (scrollY > 1000) {
+        backToTopButton.classList.add('show')
+      } else {
+        backToTopButton.classList.remove('show')
+      }
+  }
 function openMenu() {
     document.body.classList.add('menu-expanded')
   }
